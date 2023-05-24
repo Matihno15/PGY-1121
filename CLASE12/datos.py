96a2ae4b8f5c560c9f7f35ee8db8313c66d77c29
Matihno15 <134552953+Matihno15@users.noreply.github.com>
2023-05-24 16:43:16 -0400

    datos solicitados

---
+++ CLASE12/datos.py
@@ -0,0 +1,9 @@
+# Ejemplo de uso de github 
+print("Ingreso de datos")
+print("--------------------")
+nom=input("Ingrese su nombre: ")
+edad=int(input("Ingrese su edad: "))
+print("---------------------------")
+print(f"Su numbre es {nom}")
+print(f"Su edad es {edad}")
+print("Programa finalizado...")
